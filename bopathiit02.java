fc0177e1c007ebbbfc63b1139fcf2896585c0439
boopathi16it09 <47378181+boopathi16it09@users.noreply.github.com>
2019-02-06 14:21:27 +0530

    Create bopathiit02.java

---
+++ bopathiit02.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+     
+     class OddOrEven
+    {
+       public static void main(String args[])
+       {
+          int x=3;
+       
+       
+         
+          if (x % 2 == 0)
+          {
+             System.out.println("even.");
+          }
+          else
+          {
+             System.out.println(" odd.");
+          }
+       }
+    }
