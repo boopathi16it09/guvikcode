f0cacd2e9fd6da00df577218ad32bb856ff22435
boopathi16it09 <47378181+boopathi16it09@users.noreply.github.com>
2019-02-06 14:42:45 +0530

    Create count.java

---
+++ count.java
@@ -0,0 +1,33 @@
+class Count {
+
+    public static void main(String[] args) {
+        String line = "This website is aw3som2.";
+        int vowels = 0, consonants = 0, digits = 0, spaces = 0;
+
+        line = line.toLowerCase();
+        for(int i = 0; i < line.length(); ++i)
+        {
+            char ch = line.charAt(i);
+            if(ch == 'a' || ch == 'e' || ch == 'i'
+                || ch == 'o' || ch == 'u') {
+                ++vowels;
+            }
+            else if((ch >= 'a'&& ch <= 'z')) {
+                ++consonants;
+            }
+            else if( ch >= '0' && ch <= '9')
+            {
+                ++digits;
+            }
+            else if (ch ==' ')
+            {
+                ++spaces;
+            }
+        }
+
+        System.out.println("Vow: " + vowels);
+        System.out.println("Cons: " + consonants);
+        System.out.println("Dig: " + digits);
+        System.out.println("White spaces: " + spaces);
+    }
+}
