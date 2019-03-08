9401d782f25463b81c3d066cc3e0c61e15efa333
boopathi16it09 <47378181+boopathi16it09@users.noreply.github.com>
2019-03-08 11:52:07 +0530

    Create bg07.java

---
+++ bg07.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.Scanner;
+ class helloworld{
+
+    public static void main(String[] args) {
+int count=0;
+    	Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+       for(int i=0;i<n;i++)
+       {
+        System.out.println("Hello");
+       }
+        
+    }
+}
