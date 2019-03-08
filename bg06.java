406168b63d5e4794b8d8f86ec5585fd60e861980
boopathi16it09 <47378181+boopathi16it09@users.noreply.github.com>
2019-03-08 11:43:40 +0530

    Create bg06.java

---
+++ bg06.java
@@ -0,0 +1,16 @@
+import java.util.*;
+ class leap{
+
+     public static void main(String []args){
+        Scanner input=new Scanner(System.in);
+        int year=input.nextInt();
+        if(year%4==0)
+        {
+            System.out.println("yes");
+        }
+        else
+        {
+            System.out.println("no");
+        }
+     }
+}
